8fd40859c3175766c845e5dbd7dc3405c19dd5e1
Berenger <bere@nbk-10.leaf.space>
2021-12-02 07:59:17 +0100

    ⭐

---
+++ 2/solution.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+import sys
+
+
+def main():
+    f = open(sys.argv[1], "r")
+    puzzle_input = list(map(str, f.readlines()))
+    f.close()
+    commands = []
+    tmp = []
+
+    for p in puzzle_input:
+        tmp = p.strip().split(" ")
+        commands.append(tmp)
+
+    horizontal = 0
+    depth = 0
+
+    for c in commands:
+        print(c)
+        if c[0] == "forward":
+            horizontal = horizontal + int(c[1])
+        if c[0] == "down":
+            depth = depth + int(c[1])
+        if c[0] == "up":
+            depth = depth - int(c[1])
+    result = horizontal * depth
+
+    print(horizontal)
+    print(depth)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
